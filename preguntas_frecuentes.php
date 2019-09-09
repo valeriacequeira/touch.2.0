dc14380d96d81378e4bb3484450ed2751f8869d2
sofia sorhanet <sofiasorhanet@gmail.com>
2019-09-09 18:44:06 -0300

    limpie todo el codigo

---
+++ preguntas_frecuentes.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+              <title>Touch 2.0 &mdash; Citas y más</title>
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+              <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+
+
+              <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+              <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+              <link rel="stylesheet" href="bootstrap/.min.css">
+              <link rel="stylesheet" href="bootstrap/jquery-ui.css">
+              <link rel="stylesheet" href="bootstrap/owl.carousel.min.css">
+              <link rel="stylesheet" href="bootstrap/owl.theme.default.min.css">
+              
+
+              <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+              <link rel="stylesheet" href="bootstrap/aos.css">
+
+              <link rel="stylesheet" href="bootstrap/style.css">
+
+              <link rel="stylesheet" href="css_registro/estilo.css">
+
+              <link rel="stylesheet" href="css_/footer.css">
+
+
+
+
+
+              <link rel="stylesheet" href="css/estilo_registro_login.css">
+              <link rel="stylesheet" href="css/perfiles_css.css">
+
+
+         
+        </head>
+
+
+
+          <!-- navbar -->
+
+          <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+          <div class="site-wrap">
+
+            <div class="site-mobile-menu site-navbar-target">
+              <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                  <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+              </div>
+              <div class="site-mobile-menu-body"></div>
+            </div>
+
+
+            <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+              <div class="container-fluid">
+                <div class="d-flex align-items-center">
+                  <div class="site-logo mr-auto w-25"><a href="index.php">Touch 2.0</a></div>
+
+                  <div class="mx-auto text-center">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+                      <ul class="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0">
+                        <li><a href="#" class="nav-link">Nosotros</a></li>
+                        <li><a href="preguntas_frecuentes.php" class="nav-link">Preguntas frecuentes</a></li>
+                        <li><a href="#" class="nav-link">Testimonios</a></li>
+                      </ul>
+                    </nav>
+                  </div>
+
+
+                  <!-- boton de registro -->
+
+                  <div class="ml-auto w-25">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+                      <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0">
+                        <li class="cta"><a href="form_registro.php" class="nav-link"><span>Registrate</span></a></li>
+                      </ul>
+                    </nav>
+                    <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a>
+                  </div>
+                </div>
+              </div>
+
+            </header>
+
+
+            <!-- seccion intro -->
+
+            <div class="intro-section" id="home-section">
+
+                <div class="slide-1" style="background-image: url('imagen/amigosuno.jpg');" data-stellar-background-ratio="0.5">
+                  <div class="container">
+                    <div class="row align-items-center">
+                      <div class="col-12">
+                        <div class="row align-items-center">
+
+                          <div class="col-lg-6 mb-4">
+                            <h1  data-aos="fade-up" data-aos-delay="100">Conseguí tu pareja ideal.</h1>
+                            <p class="mb-4"  data-aos="fade-up" data-aos-delay="200">Quizás la encuentres a un solo clic de distancia. INTENTALO</p>
+                            <p data-aos="fade-up" data-aos-delay="300"><a href="form_registro.php" class="btn btn-primary py-3 px-5 btn-pill">Registrate</a></p>
+
+                          </div>
+                       </div>
+                     </div>
+                  </div>
+                </div>
+              </div>
+             </div>
+
+
+
+          <!-- preguntas frecuentes -->
+
+          <div class="row">
+                         <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>¿Qué es Touch2.0.com?</h2>
+                              
+                                    <div class="parrafo">
+                                       <p class= "text-light bg-dark">
+                                           Touch2.0.com es un sitio especializado en citas en línea, que ayuda a personas de Latinoamérica y a gente de orígenes occidentales a encontrar su pareja ideal. Ofrecemos un servicio acogedor que combinado a una búsqueda sofisticada y a las opciones de mensajería, harán de la búsqueda de su verdadero amor una experiencia divertida y agradable.
+                                        </p>
+                                    </div>                      
+                           </div>
+
+
+                           <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>¿Por qué utilizar Touch2.0.com?</h2>
+                              
+                                    <div class="parrafo">
+                                       <p class= "text-light bg-dark">
+                                           A diferencia de otros sitios, Touch2.0.com ofrece un servicio amigable y personalizado que se combina con la más reciente tecnología. También somos conscientes de las motivaciones y aspiraciones de aquellas personas de diferentes orígenes que buscan encontrar su pareja ideal y sentimos que nuestras experiencias pueden ser de gran ayuda. Entendemos que algunas veces... ¡la persona perfecta para usted se encuentra al otro lado del mundo! Pero no importa dónde esté esa persona especial, o por qué usted desea conocerla, nosotros podemos ayudarle a encontrar a su pareja ideal.
+                                        </p>
+                                    </div>                      
+                           </div>
+
+
+                             <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>Beneficios para los miembros de Touch2.0.com:</h2>
+                              
+                                    <div class="parrafo">
+                                       <p class= "text-light bg-dark">
+                                           Busque entre miles de perfiles de atractivos hombres y mujeres de todos los orígenes que desean encontrar a alguien como usted. Su bandeja de entrada personal le permite encontrar fácilmente y de forma anónima a la pareja perfecta. Registre un perfil al instante y agregue una foto.
+                                        </p>
+                                    </div>                      
+                           </div>
+
+
+                          
+                           <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>¿Cuánto cuesta convertirse en suscriptor?</h2>
+                              
+                                    <div class="parrafo">
+                                      <p class= "text-light bg-dark">
+                                         Absolutamente nada, es totalmente gratuito.    
+                                      </p>
+                                    </div>                      
+                           </div>
+
+                            <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>¿Cómo elimino mi perfil?</h2>
+                              
+                                    <div class="parrafo">
+                                      <p class= "text-light bg-dark">
+                                         Solo conseguiras eliminar tu perfil accediendo al sitio web por el computador. Sigue estos pasos: 1) Accede a tu perfil; 2) Clic en icono de configuración y listo!.   
+                                      </p>
+                                    </div>                      
+                           </div>
+
+
+                            <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>Olvide mi contraseña. ¿y ahora?</h2>
+                              
+                                    <div class="parrafo">
+                                      <p class= "text-light bg-dark">
+                                         Pasos para recuperarla, solo desde el computador 1) Accede a la página inicial; 2) Clic en olvidaste tu contraseña, ubicado en la parte inferior del formulario.   
+                                      </p>
+                                    </div>                      
+                           </div>
+
+                       
+                            <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>¿Cómo denuncio a un usuario?</h2>
+                              
+                                    <div class="parrafo">
+                                      <p class= "text-light bg-dark">
+                                         Para hacerlo solo tenes que hacer clic en la opcion "DENUNCIAR" dentro del perfil del usuario.
+                                      </p>
+                                    </div>                      
+                           </div>
+                             
+
+                             <div class="color1 col-xs-12 col-sm-6 col-md-4">
+                              <h2>¿Cómo debe ser la foto que voy a mandar?</h2>
+                              
+                                    <div class="parrafo">
+                                      <p class= "text-light bg-dark">
+                                         Todas las fotos ingresadas en nuestro sitio web pasan por una moderación antes de entrar a la página, debe ser imágenes que enfaticen tu rostro y no deben contener desnudos ni violencia.
+                                      </p>
+
+                                    </div>                      
+                           </div>
+
+
+      </div>
+
+                 
+        
+
+       <div class="footer">
+                  <div class="container">
+
+                      <hr>
+                          <div class="text-center center-block">
+                              <p class="txt-railway">- Touch 2.0.com -</p>
+                              <br />
+                                  <a href="https://www.facebook.com/bootsnipp"><i class="fab fa-facebook-square"></i></a>
+                                <a href="https://twitter.com/bootsnipp"><i class="fab fa-instagram"></i></a>
+                                <a href="https://plus.google.com/+Bootsnipp-page"><i class="fab fa-google-plus-g"></i></a>
+                                <a href="mailto:bootsnipp@gmail.com"><i class="fas fa-envelope-square"></i></a>
+                          </div>
+
+                    </div>
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+                  <script src="js/jquery-3.3.1.min.js"></script>
+                  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+                  <script src="js/jquery-ui.js"></script>
+                  <script src="js/popper.min.js"></script>
+                  <script src="js/bootstrap.min.js"></script>
+                  <script src="js/owl.carousel.min.js"></script>
+                  <script src="js/jquery.stellar.min.js"></script>
+                  <script src="js/jquery.countdown.min.js"></script>
+                  <script src="js/bootstrap-datepicker.min.js"></script>
+                  <script src="js/jquery.easing.1.3.js"></script>
+                  <script src="js/aos.js"></script>
+                  <script src="js/jquery.fancybox.min.js"></script>
+                  <script src="js/jquery.sticky.js"></script>
+
+                  
+                  <script src="js/main.js"></script>
+
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
