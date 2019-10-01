96c83a593c9013b8ccbc7c58383ad131e7aaa49e
sofia sorhanet <sofiasorhanet@gmail.com>
2019-10-01 12:51:33 -0300

    cambio perfil

--- perfil.php
+++ perfil.php
@@ -15,8 +15,44 @@
 </head>
 <body>
    <header>
+
 		<div class="container">
-			
+						<!-- navbar -->
+			                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			  <a class="navbar-brand" href="#">TOUCH 2.0</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav mr-auto">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Mensajes</a>
+			      </li>
+			      <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          Ajustes
+			        </a>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">Seguridad</a>
+			          <a class="dropdown-item" href="#">Privacidad</a>
+			          <div class="dropdown-divider"></div>
+			          <a class="dropdown-item" href="#">Idioma</a>
+			        </div>
+			      </li>			     
+			    </ul>
+			    <form class="form-inline my-2 my-lg-0">
+			      <input class="form-control mr-sm-2" type="seach" placeholder="buscar" aria-label="buscar">
+			      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+			    </form>
+			  </div>
+			</nav>
+
+
+
 	   
 		    <h1>Bienvenido <?=$_SESSION["nombre"]?></h1>
 		      
@@ -31,6 +67,10 @@
 
     </div>
 
+    <!-- footer -->
+
+
+    <?php require_once ("footer.php") ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
